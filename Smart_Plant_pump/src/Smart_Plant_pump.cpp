40ea2053cdc23650cca8c037483be7521f29ec2b
Laura Green <laura.eliza.green@gmail.com>
2024-02-24 16:19:12 -0700

    soil probe integrated with pump--still having trouble with slide control...maybe not bool

--- Smart_Plant_pump/src/Smart_Plant_pump.cpp
+++ Smart_Plant_pump/src/Smart_Plant_pump.cpp
@@ -12,20 +12,24 @@
 #include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
 #include "Adafruit_MQTT/Adafruit_MQTT.h"
 #include "credentials.h"
-
-TCPClient TheClient;//global state
+/*
+TCPClient TheClient;//global state (defines transmission control protocol connection)
 //Sets up MQTT client class
-Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);//object that defines connection
 
 //FEEDS
-Adafruit_MQTT_Subscribe subFeedPumpButton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/waterPlant"); 
+Adafruit_MQTT_Subscribe subFeedPumpButton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/waterPlant"); //object for each feed
 Adafruit_MQTT_Publish pubFeedSoilMoisture = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/soilMoisture");
-
+*/
+//declare variables
 const int PUMPPIN=D7;//digital pin to turn pump on/off
 const int SOILPIN=A1;//analog pin for soil probe
-unsigned int lastTime;//for publishing time
-float subValue,pubValue;
-int soilMoisture;
+//unsigned int SOILPUBTIME;//for publishing time for soil sensor feed
+unsigned const int WATERTIME=1000;//how long to water after switched to on?
+unsigned const int SOILREAD=5000;//how often to read probe
+unsigned int pumpTime;
+unsigned int soilTime; 
+int soilMoisture;//variable for soil data collected (to be published)
 bool buttonState;//from Dashboard waterPlant feed
 
 
@@ -46,44 +50,88 @@ void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected,10000);
 
-  // Connect to Internet but not Particle Cloud
+  /* Connect to Internet but not Particle Cloud  ***why not cloud??
   WiFi.on();
   WiFi.connect();
   while(WiFi.connecting()) {
     Serial.printf(".");
   }
   Serial.printf("\n\n");
+  */
 
   // Setup MQTT subscription
-  mqtt.subscribe(&subFeedPumpButton);
+  //mqtt.subscribe(&subFeedPumpButton);
+
   pinMode(PUMPPIN,OUTPUT);
   pinMode(SOILPIN,INPUT);
-  buttonState=LOW;
+
+  buttonState=LOW;//initial button state
+  //pumpTime=millis();
+  soilTime=millis();
+  //pumpTime=millis();
+  
 }
   
 
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+  /*
   MQTT_connect();
   MQTT_ping();
 //wait for incoming subscription packets
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(100))) {//subscription requires void setup to 1. subscribe anytime feed on cloud changes new data-let's you know-in setup
-    if (subscription == &subFeedButtonOnOff) {//look at subscription feed for 1/10 sec to see if somethings there associated with feed created
+    if (subscription == &subFeedPumpButton) {//look at subscription feed for 1/10 sec to see if somethings there associated with feed created
       buttonState = atof((char *)subFeedPumpButton.lastread);//take last read and convert to ASCI from 0s and 1s and convert that to float.(or to integer)
     }
   }
-  //manual control of water pump
-  if(buttonState==HIGH){
+  //manual control of water pump from smart plant dashboard
+  if(buttonState==HIGH){//put timer here for watering time
  digitalWrite(PUMPPIN,HIGH);
  Serial.printf("WATERING PLANT\n");
 }
 else{
   digitalWrite(PUMPPIN,LOW);
 }
+*/
+///*
 
+
+if(millis()-soilTime>SOILREAD) {
 soilMoisture=analogRead(SOILPIN);
+Serial.printf("soil moisture is %i\n",soilMoisture);//add publishing here too
+soilTime=millis();
+if(soilMoisture>1500) { 
+  pumpTime=millis();//can't set this initially in setup bc could be a long time before soil is dry...
+}
+}
+
+  while(millis()-pumpTime<WATERTIME) {
+    digitalWrite(PUMPPIN,HIGH);
+     
+}
+digitalWrite(PUMPPIN,LOW);
+  
+     
+  }
+  
+
+/*timer for publishing soil data? or maybe just publish when read (timed there)
+if((millis()-lastTime > SOILPUBGAP)) {//publishing (how often?)
+    if(mqtt.Update()) {
+      pubFeedSoilMoisture.publish(soilMoisture);
+      Serial.printf("Publishing %i \n",soilMoisture); 
+      } 
+    lastTime = millis();
+  }
+  */
+
+  
+
+  
+  
+  /*
 //publishing soil probe date to feed
 if((millis()-lastTime > 10000)) {//publishing (how often?)
     if(mqtt.Update()) {
@@ -92,27 +140,12 @@ if((millis()-lastTime > 10000)) {//publishing (how often?)
       } 
     lastTime = millis();
   }
+  
 }
-void MQTT_connect() {//actually connects to server, if not connected stuck in loop
-  int8_t ret;//photon 2 thinks of integers as 32bits
- 
-  // Return if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
- 
-  Serial.print("Connecting to MQTT... ");
- 
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
-       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds and try again
-  }
-  Serial.printf("MQTT Connected!\n");
-}
-///Declare functions
+*/
 
+///Declare functions
+/*
 void MQTT_connect() {//actually connects to server, if not connected stuck in loop
   int8_t ret;//photon 2 thinks of integers as 32bits
  
@@ -149,6 +182,7 @@ bool MQTT_ping() {//broker will disconnect if doesn't hear anything, just remind
   }
   return pingStatus;
 }
+*/
 
 
 
